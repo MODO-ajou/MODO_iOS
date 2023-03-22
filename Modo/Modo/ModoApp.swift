07bdc9a121f39824f3efdfbf92700202e1f5046b
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-03-22 15:31:56 +0900

    make Modo iOS Project

---
+++ Modo/Modo/ModoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModoApp.swift
+//  Modo
+//
+//  Created by MacBook on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ModoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

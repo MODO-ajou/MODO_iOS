7e744178489e107e3fc96079ad5c09867cdf0c0e
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-03-23 21:03:53 +0900

    add test button

--- Modo/Modo/ContentView.swift
+++ Modo/Modo/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Button(action: {
+                //
+            }) {
+                Text("Modo")
+            }
             Text("Hello, world!")
         }
         .padding()

c6eb19abeca2b3ff5e31a1e8516673fd3bef1ce4
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-03-25 17:37:15 +0900

    [Feat] 프로젝트 세팅 (MODO-18)

---
+++ Modo/Modo/Configuration/Extensions/Ex+View.swift
@@ -0,0 +1,29 @@
+//
+//  EX+View.swift
+//  Modo
+//
+//  Created by MacBook on 2023/03/25.
+//
+import Foundation
+import SwiftUI
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+    
+    // MARK: - 다른 화면 터치 시 키보드가 내려감
+    func endTextEditing() {
+        UIApplication.shared.sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil, from: nil, for: nil
+        )
+    }
+}
